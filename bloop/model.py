0331d2b66d083bfd52570f0fc050be49014154a5
Joe Cross <joe.mcross@gmail.com>
2015-02-03 23:34:38 -0800

    BaseModel meta keys roughly namespaces

--- bloop/model.py
+++ bloop/model.py
@@ -1,5 +1,6 @@
+import bloop.index
 import bloop.column
-import bloop.dynamo_util
+import bloop.dynamo
 import declare
 missing = object()
 
@@ -10,19 +11,17 @@ class __BaseModel(object):
     up the correct metaclass.
     '''
     def __init__(self, **attrs):
-        columns = self.__meta__["columns_by_name"]
-        for name, column in columns.items():
-            value = attrs.get(name, missing)
-            # Missing expected column
-            if value is missing:
-                continue
-            setattr(self, name, value)
+        columns = self.__meta__["dynamo.columns"]
+        for column in columns:
+            value = attrs.get(column.model_name, missing)
+            if value is not missing:
+                setattr(self, column.model_name, value)
 
     @classmethod
     def __load__(cls, values):
         ''' dict -> obj '''
         meta = cls.__meta__
-        columns = meta["columns"]
+        columns = meta["dynamo.columns"]
         init = meta["bloop.init"]
         engine = meta["bloop.engine"].type_engine
         attrs = {}
@@ -37,7 +36,7 @@ class __BaseModel(object):
     def __dump__(cls, obj):
         ''' obj -> dict '''
         meta = cls.__meta__
-        columns = meta["columns"]
+        columns = meta["dynamo.columns"]
         engine = meta["bloop.engine"].type_engine
         attrs = {}
         for column in columns:
@@ -56,14 +55,40 @@ def BaseModel(engine):
     class ModelMetaclass(declare.ModelMetaclass):
         def __new__(metaclass, name, bases, attrs):
             model = super().__new__(metaclass, name, bases, attrs)
+            meta = model.__meta__
 
-            # Load and index columns
+            # Load columns, hash_key, range_key
             # ----------------------------------------------------------
             is_column = lambda field: isinstance(field, bloop.column.Column)
             columns = list(filter(is_column, model.__meta__['fields']))
-            meta = model.__meta__
-            meta['columns'] = columns
-            meta['columns_by_name'] = declare.index(columns, 'model_name')
+            meta['dynamo.columns'] = columns
+            meta['dynamo.columns.by.model_name'] = declare.index(
+                columns, 'model_name')
+            meta['dynamo.columns.by.dynamo_name'] = declare.index(
+                columns, 'dynamo_name')
+
+            for column in columns:
+                if column.hash_key:
+                    meta['dynamo.table.hash_key'] = column
+                    break
+            else:
+                raise AttributeError(
+                    "Must define a hash key for model {}".format(
+                        model.__name__))
+            for column in columns:
+                if column.range_key:
+                    meta['dynamo.table.range_key'] = column
+                    break
+
+            # Load indexes
+            # ----------------------------------------------------------
+            indexes = []
+            for key, value in attrs.items():
+                if isinstance(value, bloop.index.Index):
+                    value.model_name = key
+                    value.model = model
+                    indexes.append(value)
+            meta['dynamo.indexes'] = indexes
 
             # Entry point for model population.  By default this is the
             # model class.  Custom subclasses of the engine's
@@ -74,9 +99,12 @@ def BaseModel(engine):
             meta["bloop.init"] = meta.get("bloop.init", model)
             meta["bloop.engine"] = engine
 
-            meta["table_name"] = meta.get("table_name", model.__name__)
-            meta["write_units"] = meta.get("write_units", 1)
-            meta["read_units"] = meta.get("read_units", 1)
+            meta["dynamo.table.name"] = meta.get(
+                "dynamo.table.name", model.__name__)
+            meta["dynamo.table.write_units"] = meta.get(
+                "dynamo.table.write_units", 1)
+            meta["dynamo.table.read_units"] = meta.get(
+                "dynamo.table.read_units", 1)
 
             # If the engine already has a base, register this model.
             # Otherwise, this probably IS the engine's base model

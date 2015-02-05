1b4c78175e2a0b28e4a72745862c9eaac8108274
Joe Cross <joe.mcross@gmail.com>
2015-02-04 18:17:58 -0800

    Actually update column

--- bloop/column.py
+++ bloop/column.py
@@ -5,14 +5,29 @@ _meta_key = "__column_meta_{}".format(uuid.uuid4().hex)
 
 
 class Column(declare.Field):
-    def __init__(self, *args, **kwargs):
-        self.hash_key = kwargs.pop('hash_key', False)
-        self.range_key = kwargs.pop('range_key', False)
-        self._dynamo_name = kwargs.pop('name', missing)
+    def __init__(self, *args, hash_key=None, range_key=None,
+                 name=missing, **kwargs):
+        self._hash_key = hash_key
+        self._range_key = range_key
+        self._dynamo_name = name
 
         self.column_key = "__{}_{}".format(
             self.__class__.__name__, uuid.uuid4().hex)
         super().__init__(*args, **kwargs)
+
+    @property
+    def hash_key(self):
+        return self._hash_key
+
+    @property
+    def range_key(self):
+        return self._range_key
+
+    @property
+    def dynamo_name(self):
+        if self._dynamo_name is missing:
+            return self.model_name
+        return self._dynamo_name
 
     def __meta__(self, obj):
         ''' Return the column-specific metadata dict for a given object '''
@@ -48,39 +63,23 @@ class Column(declare.Field):
         obj_meta[name] = value
         return value
 
+
+class Index(Column):
+    def __init__(self, *args, projection='KEYS_ONLY', **kwargs):
+        super().__init__(*args, **kwargs)
+        self.projection = projection
+
+
+class GlobalSecondaryIndex(Index):
+    def __init__(self, *args, write_units=1, read_units=1, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.write_units = write_units
+        self.read_units = read_units
+
+
+class LocalSecondaryIndex(Index):
+    ''' when constructing a model, you MUST set this index's model attr. '''
     @property
-    def dynamo_name(self):
-        if self._dynamo_name is missing:
-            return self.model_name
-        return self._dynamo_name
-
-
-class DirtyColumn(Column):
-    def is_dirty(self, obj):
-        return self.meta_get(obj, 'dirty', default=False)
-
-    def mark_dirty(self, obj):
-        return self.meta_set(obj, 'dirty', True)
-
-    def set(self, obj, value):
-        if not self.is_dirty(obj):
-            try:
-                original = self.get(obj)
-            except AttributeError:
-                # If we couldn't load before, must be a mutation
-                self.mark_dirty(obj)
-            else:
-                # If this value is different it's a mutation
-                if value != original:
-                    self.mark_dirty(obj)
-        super().set(obj, value)
-
-    def delete(self, obj):
-        try:
-            super().delete(obj)
-        except AttributeError:
-            # If we couldn't delete then it was never set
-            raise
-        else:
-            # Successful deletion is a mutation
-            self.mark_dirty(obj)
+    def hash_key(self):
+        hash_column = self.model.__meta__['dynamo.table.hash_key']
+        return hash_column.dynamo_name

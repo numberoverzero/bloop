4a6f3c8d0179c7d1c2a9ec34ef632ffa015201ff
Joe Cross <joe.mcross@gmail.com>
2015-02-04 18:15:19 -0800

    Index inherits from Column

--- bloop/model.py
+++ bloop/model.py
@@ -1,109 +1,85 @@
-import bloop.column
-import bloop.dynamo
 import declare
+import uuid
 missing = object()
+_meta_key = "__column_meta_{}".format(uuid.uuid4().hex)
 
 
-class __BaseModel(object):
-    '''
-    do not subclass directly.  use `base_model` which sets
-    up the correct metaclass.
-    '''
-    def __init__(self, **attrs):
-        columns = self.__meta__["dynamo.columns"]
-        for column in columns:
-            value = attrs.get(column.model_name, missing)
-            if value is not missing:
-                setattr(self, column.model_name, value)
+class Column(declare.Field):
+    def __init__(self, *args, hash_key=None, range_key=None,
+                 name=missing, **kwargs):
+        self._hash_key = hash_key
+        self._range_key = range_key
+        self._dynamo_name = name
 
-    @classmethod
-    def __load__(cls, values):
-        ''' dict -> obj '''
-        meta = cls.__meta__
-        columns = meta["dynamo.columns"]
-        init = meta["bloop.init"]
-        engine = meta["bloop.engine"].type_engine
-        attrs = {}
-        for column in columns:
-            value = values.get(column.dynamo_name, missing)
-            # Missing expected column
-            if value is not missing:
-                attrs[column.model_name] = engine.load(column.typedef, value)
-        return init(**attrs)
+        self.column_key = "__{}_{}".format(
+            self.__class__.__name__, uuid.uuid4().hex)
+        super().__init__(*args, **kwargs)
 
-    @classmethod
-    def __dump__(cls, obj):
-        ''' obj -> dict '''
-        meta = cls.__meta__
-        columns = meta["dynamo.columns"]
-        engine = meta["bloop.engine"].type_engine
-        attrs = {}
-        for column in columns:
-            value = getattr(obj, column.model_name, missing)
-            # Missing expected column
-            if value is not missing:
-                attrs[column.dynamo_name] = engine.dump(column.typedef, value)
-        return attrs
+    @property
+    def hash_key(self):
+        return self._hash_key
+
+    @property
+    def range_key(self):
+        return self._range_key
+
+    @property
+    def dynamo_name(self):
+        if self._dynamo_name is missing:
+            return self.model_name
+        return self._dynamo_name
+
+    def __meta__(self, obj):
+        ''' Return the column-specific metadata dict for a given object '''
+        meta = obj.__dict__.get(_meta_key, None)
+        if meta is None:
+            meta = obj.__dict__[_meta_key] = {}
+        column_meta = meta.get(self.column_key, None)
+        if column_meta is None:
+            column_meta = meta[self.column_key] = {}
+        return column_meta
+
+    def meta_get(self, obj, name, default=missing):
+        '''
+        look up and return the value of a property in the column metadata,
+        setting and return the default value if specified.
+
+        if `default` is not set, KeyError is raised and the metadata dict is
+        not mutated.
+        '''
+        obj_meta = self.__meta__(obj)
+        value = obj_meta.get(name, missing)
+        # Failed to find - either set and return default, or raise
+        if value is missing:
+            # Don't mutate on fail to find
+            if default is missing:
+                raise KeyError("Unknown column meta property {}".format(name))
+            else:
+                value = obj_meta[name] = default
+        return value
+
+    def meta_set(self, obj, name, value):
+        obj_meta = self.__meta__(obj)
+        obj_meta[name] = value
+        return value
 
 
-def BaseModel(engine):
-    '''
-    A metaclass per engine is necessary so that subclasses of __BaseModel
-    are correctly registered with the engine
-    '''
-    class ModelMetaclass(declare.ModelMetaclass):
-        def __new__(metaclass, name, bases, attrs):
-            model = super().__new__(metaclass, name, bases, attrs)
-            meta = model.__meta__
+class Index(Column):
+    def __init__(self, *args, projection='KEYS_ONLY', **kwargs):
+        super().__init__(*args, **kwargs)
+        self.projection = projection
 
-            # Load columns, indexes, hash_key, range_key
-            # ----------------------------------------------------------
-            is_column = lambda field: isinstance(field, bloop.column.Column)
-            is_index = lambda field: isinstance(field, bloop.column.Index)
-            columns = list(filter(is_column, model.__meta__['fields']))
-            indexes = list(filter(is_index, columns))
 
-            # Remove indexes from columns since they're treated differently
-            for index in indexes:
-                index.model = model
-                columns.remove(index)
+class GlobalSecondaryIndex(Index):
+    def __init__(self, *args, write_units=1, read_units=1, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.write_units = write_units
+        self.read_units = read_units
 
-            meta['dynamo.indexes'] = indexes
-            meta['dynamo.columns'] = columns
-            meta['dynamo.columns.by.model_name'] = declare.index(
-                columns, 'model_name')
-            meta['dynamo.columns.by.dynamo_name'] = declare.index(
-                columns, 'dynamo_name')
 
-            for column in columns:
-                if column.hash_key:
-                    meta['dynamo.table.hash_key'] = column
-                    break
-
-            for column in columns:
-                if column.range_key:
-                    meta['dynamo.table.range_key'] = column
-                    break
-
-            # Entry point for model population.  By default this is the
-            # model class.  Custom subclasses of the engine's
-            # base model should specify the meta property "bloop.init",
-            # which should be a function taking a **kwarg of name:value
-            # pairs corresponding to modeled columns.
-
-            meta["bloop.init"] = meta.get("bloop.init", model)
-            meta["bloop.engine"] = engine
-
-            meta["dynamo.table.name"] = meta.get(
-                "dynamo.table.name", model.__name__)
-            meta["dynamo.table.write_units"] = meta.get(
-                "dynamo.table.write_units", 1)
-            meta["dynamo.table.read_units"] = meta.get(
-                "dynamo.table.read_units", 1)
-
-            # If the engine already has a base, register this model.
-            # Otherwise, this probably IS the engine's base model
-            if engine.model:
-                engine.register(model)
-            return model
-    return ModelMetaclass("Model", (__BaseModel,), {})
+class LocalSecondaryIndex(Index):
+    ''' when constructing a model, you MUST set this index's model attr. '''
+    @property
+    def hash_key(self):
+        hash_column = self.model.__meta__['dynamo.table.hash_key']
+        return hash_column.dynamo_name

109fc84cb307083f6a01317bb5b5bea0578088d3
Joe Cross <joe.mcross@gmail.com>
2015-03-30 15:41:32 -0700

    Add UUID to bloop __all__

--- bloop/__init__.py
+++ bloop/__init__.py
@@ -1,13 +1,13 @@
 from bloop.engine import Engine, ObjectsNotFound, ConstraintViolation
 from bloop.column import Column, GlobalSecondaryIndex, LocalSecondaryIndex
 from bloop.types import (
-    String, Float, Integer, Binary, StringSet, FloatSet,
+    String, UUID, Float, Integer, Binary, StringSet, FloatSet,
     IntegerSet, BinarySet, Null, Boolean, Map, List
 )
 
 __all__ = [
     "Engine", "ObjectsNotFound", "ConstraintViolation",
     "Column", "GlobalSecondaryIndex", "LocalSecondaryIndex",
-    "String", "Float", "Integer", "Binary", "StringSet", "FloatSet",
+    "String", "UUID", "Float", "Integer", "Binary", "StringSet", "FloatSet",
     "IntegerSet", "BinarySet", "Null", "Boolean", "Map", "List"
 ]

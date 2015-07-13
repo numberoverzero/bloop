a8431449ebef247cca40606aff3f29dad6dbb6be
Joe Cross <joe.mcross@gmail.com>
2015-07-12 18:50:56 -0700

    Simplify binding engines locally

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,44 @@
-import boto3.session
-import pytest
 import bloop
+import pytest
+
+
+def noop(*a, **kw):
+    pass
 
 
 @pytest.fixture
 def session():
-    return boto3.session.Session(region_name='us-west-2',
-                                 aws_access_key_id='ACCESS_KEY',
-                                 aws_secret_access_key='SECRET_KEY')
+    class Client:
+        pass
+
+    class Session:
+        def client(self, name):
+            assert name == "dynamodb"
+            return Client()
+    return Session()
 
 
 @pytest.fixture
 def engine(session):
     return bloop.Engine(session=session)
+
+
+@pytest.fixture
+def local_bind(engine):
+    engine.client.create_table = noop
+    engine.client.validate_table = noop
+
+
+@pytest.fixture
+def User(engine, local_bind):
+    class User(engine.model):
+        id = bloop.Column(bloop.UUID, hash_key=True)
+        age = bloop.Column(bloop.Integer)
+        name = bloop.Column(bloop.String)
+        email = bloop.Column(bloop.String)
+        joined = bloop.Column(bloop.DateTime)
+
+        by_email = bloop.GlobalSecondaryIndex(hash_key='email',
+                                              projection='all')
+    engine.bind()
+    return User

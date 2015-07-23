ce3a1341b36e4375c39df075b8b73fb95dd3636f
Joe Cross <joe.mcross@gmail.com>
2015-07-23 00:39:28 -0700

    Add alabaster theme options

--- docs/conf.py
+++ docs/conf.py
@@ -125,7 +125,13 @@ html_theme = 'alabaster'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-# html_theme_options = {}
+html_theme_options = {
+    'github_user': 'numberoverzero',
+    'github_repo': 'bloop',
+    'github_banner': True,
+    'travis_button': True,
+    'show_powered_by': False
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = [alabaster.get_path()]

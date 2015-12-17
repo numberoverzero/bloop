fe6849fdc87139026889ec1e2894f9de3247a042
Joe Cross <joe.mcross@gmail.com>
2015-12-17 14:17:19 -0800

    Implement TypedMap

--- bloop/__init__.py
+++ bloop/__init__.py
@@ -7,13 +7,13 @@ from bloop.exceptions import (
 from bloop.index import GlobalSecondaryIndex, LocalSecondaryIndex
 from bloop.types import (
     Boolean, Binary, DateTime, Float, Integer,
-    List, Map, Set, String, UUID
+    List, Map, Set, String, TypedMap, UUID
 )
 
 __all__ = [
     "Boolean", "Binary", "Column", "Condition", "ConstraintViolation",
     "DateTime", "Engine", "Float", "GlobalSecondaryIndex", "Integer",
     "List", "LocalSecondaryIndex", "Map", "NotModified", "Set",
-    "String", "TableMismatch", "UnboundModel", "UUID"
+    "String", "TableMismatch", "TypedMap", "UnboundModel", "UUID"
 ]
-__version__ = "0.9.2"
+__version__ = "0.9.3"

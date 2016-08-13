2229ca29fa299a7189a16829fecb00732149699b
Joe Cross <joe.mcross@gmail.com>
2016-08-13 16:01:47 -0700

    Still need to fix renames for tests; all out of order #60

--- scripts/individual_coverage.sh
+++ scripts/individual_coverage.sh
@@ -5,9 +5,9 @@ rm -f .coverage
 for filename in bloop/*.py; do
     # strip directory
     module=$(basename $filename)
-    # strip .py
+    # skip root import file
     [[ "$module" == "__init__.py" ]] && continue
     echo "Collecting ${module%.*}..."
-    coverage run --append --branch --source=bloop/$module -m py.test tests/test_$module &> /dev/null 2>&1
+    coverage run --append --branch --source=bloop/$module -m py.test tests/unit/test_$module &> /dev/null 2>&1
 done
 coverage report -m

9c1dedfd2cc3c16a8798bc7351d8c3c699091e90
Joe Cross <joe.mcross@gmail.com>
2016-08-14 01:21:50 -0700

    Extract reusable components from search iter __next__ test #60

--- tests/unit/test_search.py
+++ tests/unit/test_search.py
@@ -5,6 +5,38 @@ from bloop.util import Sentinel
 
 from ..helpers.models import User
 
+proceed = Sentinel("proceed")
+
+
+def response(count, terminate=False):
+    """This fills in the required response structure from a single query/scan call:
+
+    Count, ScannedCount
+        required fields but not important.
+    LastEvaluatedKey
+        tells the iterator if there are more pages available.  When it's not falsey,
+        it should be fed directly back into the next request's "ExclusiveStartKey".
+    Item
+        is the number of items in this page.  By passing 0, 1, or 2 we can verify that the buffer is
+        fully drained before the next page is loaded.  It also lets us verify that the while loop will
+        follow LastEvaluatedKeys until it hits a non-empty page.
+    """
+    return {
+        "Count": count,
+        "ScannedCount": count * 3,
+        "Items": [True] * count,
+        "LastEvaluatedKey": None if terminate else proceed
+    }
+
+
+def build_responses(chain):
+    """This expands a compact integer description of a set of pages into the appropriate response structure.
+
+    For example: [0, 2, 1] expands into (0 results, proceed) -> (2 results, proceed) -> (1 result, stop).
+    We'll also use those integers in the verifier to compare number of results and number of calls.
+    """
+    return [response(count) for count in chain[:-1]] + [response(chain[-1], terminate=True)]
+
 
 def simple_iter(session, cls=SearchIterator):
     return cls(
@@ -29,6 +61,11 @@ def test_search_repr():
         (True, True,  "<Class[Model.by_gsi]>"),
     ]:
         assert search_repr(cls, has_model and model, has_index and index) == expected
+
+
+def test_iterator_returns_self(session):
+    iterator = simple_iter(session)
+    assert iterator is iter(iterator)
 
 
 def test_iterator_reset(session):
@@ -94,63 +131,41 @@ def test_iterator_next_limit_reached(session):
 
 @pytest.mark.parametrize("iterator_cls", [QueryIterator, ScanIterator])
 def test_next_states(session, iterator_cls):
-    """This monster tests all of the effective cycles for the __next__ machinery.
+    """This monster tests all of the buffer management in SearchIterator.__next__"""
 
-    As such, it can be daunting.  Here's a quick rundown, ordered by appearance:
-
-    Sentinels:
-        Placeholders for values.  This ensures the iterator doesn't depend on the structure of the
-        items and continuation token.  If it did, these would almost certainly break it.
-    response:
-        This fills in the required response structure from a single query/scan call:
-            "Count", "ScannedCount" are required fields but not important.
-            "LastEvaluatedKey" tells the iterator if there are more pages available.  When it's not falsey,
-                               it should be fed directly back into the next request's "ExclusiveStartKey".
-            "Item" is the number of items in this page.  By passing 0, 1, or 2 we can verify that the buffer is
-                   fully drained before the next page is loaded.  It also lets us verify that the while loop will
-                   follow LastEvaluatedKeys until it hits a non-empty page.
-    build_responses:
-        This expands a compact integer description of a set of pages into the appropriate response structure.
-        For example: [0, 2, 1] expands into (0 results, proceed) -> (2 results, proceed) -> (1 result, stop).
-        We'll also use those integers in the verifier to compare number of results and number of calls.
-    calls_for_current_steps:
-        The number of calls that are required to iterate the given chain in the given number of steps.
-        For example, the chain [3, 0, 1, 4] has the following table:
-            ===== =====
-            steps calls
-            ===== =====
-              0     1
-              1     1
-              2     1
-              3     1
-              4     3
-              5     4
-              6     4
-              7     4
-              8     4
-              9     4
-            ===== =====
-    verify_iterator:
-        Build a new iterator for the given chain, load the responses into the mock session, and then
-        carefully advance the iterator until it raises StopIteration.  At each step, check that the correct number
-        of calls have been made to session.search_items.
-    """
-
-    data = Sentinel("data")
-    proceed = Sentinel("proceed")
-
-    def response(count, terminate=False):
-        return {
-            "Count": count,
-            "ScannedCount": count * 3,
-            "Items": [data] * count,
-            "LastEvaluatedKey": None if terminate else proceed
-        }
-
-    def build_responses(chain):
-        return [response(count) for count in chain[:-1]] + [response(chain[-1], True)]
+    # Here are the possible boundaries for pagination:
+    #
+    #                  # calls results
+    # 0 Results        # --+-- ---+---
+    #   [           ]  #   1      0
+    #   [     |     ]  #   2      0
+    # 1 Result         # --+-- ---+---
+    #   [     ✔     ]  #   1      1
+    #   [     |  ✔  ]  #   2      1
+    #   [ ✔   |     ]  #   2      1
+    # N Results        # --+-- ---+---
+    #   [    ✔ ✔    ]  #   1      N
+    #   [     | ✔ ✔ ]  #   2      N
+    #   [  ✔  |  ✔  ]  #   2      N
+    #   [ ✔ ✔ |     ]  #   2      N
+    # Additional calls are repetitions of the above
+    chains = [
+        [0], [0, 0],
+        [1], [0, 1], [1, 0],
+        [2], [0, 2], [1, 1], [2, 0]
+    ]
 
     def calls_for_current_steps(chain, current_steps):
+        """The number of dynamodb calls that are required to iterate the given chain in the given number of steps.
+
+        For example, the chain [3, 0, 1, 4] has the following table:
+
+            +-------+---+---+---+---+---+---+---+---+---+---+
+            | steps | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |
+            +-------+---+---+---+---+---+---+---+---+---+---+
+            | calls | 1 | 1 | 1 | 1 | 3 | 4 | 4 | 4 | 4 | 4 |
+            +-------+---+---+---+---+---+---+---+---+---+---+
+        """
         required_steps = 0
         call_count = 0
         for call_count, page in enumerate(chain):
@@ -160,39 +175,27 @@ def test_next_states(session, iterator_cls):
         return call_count + 1
 
     def verify_iterator(chain):
+        """For a given sequence of result page sizes, verify that __next__ steps forward as expected.
+
+        1) Build a new iterator for the given chain
+        2) Load the responses into the mock session
+        3) Advance the iterator until it raises StopIteration.  Each step, make sure the iterator is only
+           calling dynamodb when the buffer is empty, and that it follows continue tokens on empty pages.
+        """
         iterator = simple_iter(session, cls=iterator_cls)
-        # VERY IMPORTANT!  Without the reset, calls from the previous chain
-        # will count against this chain.
+        # VERY IMPORTANT!  Without the reset, calls from
+        # the previous chain will count against this chain.
         session.search_items.reset_mock()
         session.search_items.side_effect = build_responses(chain)
 
         current_steps = 0
-        result = data
-        while result is data:
+        while next(iterator, None):
             current_steps += 1
-            result = next(iterator, None)
             expected_call_count = calls_for_current_steps(chain, current_steps)
             assert session.search_items.call_count == expected_call_count
-        assert iterator.yielded == current_steps - 1
+        assert iterator.yielded == sum(chain)
+        assert iterator.exhausted
 
-    # Here are the possible boundaries for pagination:
-    # 0 Results
-    #   [   ]  # 1 call
-    #   [ | ]  # 2 call
-    # 1 Results
-    #   [   ✔   ] # 1 call
-    #   [   | ✔ ] # 2 calls
-    #   [ ✔ |   ] # 2 calls
-    # N Results
-    #   [  ✔₁   ✔₂  ] # 1 call
-    #   [   | ✔₁ ✔₂ ] # 2 calls
-    #   [ ✔₁  |  ✔₂ ] # 2 calls
-    #   [ ✔₁ ✔₂ |   ] # 2 calls
-    # Additional calls are repetitions of the above
-    chains = [
-        [0], [0, 0],
-        [1], [0, 1], [1, 0],
-        [2], [0, 2], [1, 1], [2, 0]
-    ]
+    # Kick it all off
     for chain in chains:
         verify_iterator(chain)

daed049a9965908a9aa926dba71ac464815cbf96
Joe Cross <joe.mcross@gmail.com>
2016-10-27 19:18:24 -0700

    Add missing public api to bloop/__init__

--- bloop/__init__.py
+++ bloop/__init__.py
@@ -4,9 +4,12 @@ from .exceptions import (
     BloopException,
     ConstraintViolation,
     MissingObjects,
+    RecordsExpired,
+    ShardIteratorExpired,
     TableMismatch,
 )
 from .models import BaseModel, Column, GlobalSecondaryIndex, LocalSecondaryIndex
+from .search import QueryIterator, ScanIterator
 from .stream import Stream, stream_for
 from .types import (
     UUID,
@@ -23,9 +26,15 @@ from .types import (
 
 
 __all__ = [
-    "BaseModel", "Boolean", "Binary", "BloopException", "Column",
-    "Condition", "ConstraintViolation", "DateTime", "Engine", "Float", "GlobalSecondaryIndex", "Integer",
-    "List", "LocalSecondaryIndex", "Map", "MissingObjects", "Set", "Stream", "String", "TableMismatch",
-    "UUID", "stream_for"
+    # Models
+    "BaseModel", "Boolean", "Binary", "Column", "DateTime", "Engine", "Float", "GlobalSecondaryIndex", "Integer",
+    "List", "LocalSecondaryIndex", "Map", "Set", "String", "UUID",
+
+    # Exceptions
+    "BloopException", "ConstraintViolation", "MissingObjects",
+    "RecordsExpired", "ShardIteratorExpired", "TableMismatch",
+
+    # Misc
+    "Condition", "QueryIterator", "ScanIterator", "Stream", "stream_for"
 ]
 __version__ = "1.0.0"

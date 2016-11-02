e333890bafc75b10881e660a89434c1d9e8dd753
Joe Cross <joe.mcross@gmail.com>
2016-11-02 00:56:35 -0700

    More style tweaks

--- docs/conf.py
+++ docs/conf.py
@@ -44,8 +44,10 @@ html_static_path = ["_static"]
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_context = {
+    "css_files": ["_static/css/bloop.css"],
+    "docstitle": False,
     "favicon": "favicon-cog.ico",
-    "css_files": ["_static/css/bloop.css"]
+    "show_sphinx": False,
 }
 
 intersphinx_mapping = {

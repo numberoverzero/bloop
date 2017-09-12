c6dd15e45a5994ac1bb086c41a68eda66679a7e5
Joe Cross <joe.mcross@gmail.com>
2017-09-11 20:16:30 -0700

    Add integ test for table query with count projection #95

--- tests/integ/test_queries.py
+++ tests/integ/test_queries.py
@@ -14,3 +14,16 @@ def test_query_with_projection(engine):
 
     result = query.one()
     assert not hasattr(result, "profile")
+
+
+def test_scan_count(engine):
+    engine.bind(User)
+    for _ in range(7):
+        engine.save(valid_user())
+    scan = engine.scan(User, projection="count")
+    assert scan.count == 0
+    try:
+        next(scan)
+    except StopIteration:
+        pass
+    assert scan.count == 7
